9d54c3ed48b4a06e601fbd14e330f928218d6c00
Cééesar Henrique <cesaarhvb@gmail.com>
2024-09-06 17:17:05 -0300

    refactor: add imports for TasksModule and DatabaseModule in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TasksModule } from './entities/tasks/tasks.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    TasksModule,
+  ],
   controllers: [],
   providers: [],
 })
