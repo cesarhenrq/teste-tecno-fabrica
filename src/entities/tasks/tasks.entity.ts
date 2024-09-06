3753f1c21790c954ec6be77562436a6b3b5a2d43
Cééesar Henrique <cesaarhvb@gmail.com>
2024-09-06 17:18:13 -0300

    feat: add Tasks entity to handle task data

---
+++ src/entities/tasks/tasks.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { StatusEnum } from 'src/enums/status.enum';
+
+@Entity({
+  name: 'tasks',
+})
+export class Tasks {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false })
+  title: string;
+
+  @Column({ nullable: false })
+  description: string;
+
+  @Column({
+    type: 'enum',
+    nullable: false,
+    enum: StatusEnum,
+    default: StatusEnum.PENDING,
+  })
+  status: StatusEnum;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+}
