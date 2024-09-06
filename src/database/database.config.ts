1e2d3a8e6cc9e96ba2990cf691a57f7f562bbd58
Cééesar Henrique <cesaarhvb@gmail.com>
2024-09-06 15:14:33 -0300

    chore: add initial configure

---
+++ src/database/database.config.ts
@@ -0,0 +1,27 @@
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+export default <TypeOrmModuleAsyncOptions>{
+  imports: [ConfigModule],
+  inject: [ConfigService],
+
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => {
+    return <PostgresConnectionOptions>{
+      type: 'postgres',
+      host: configService.get('DB_HOST'),
+      port: +configService.get('DB_PORT'),
+      username: configService.get('DB_USERNAME'),
+      password: configService.get('DB_PASSWORD'),
+      database: configService.get('DB_DATABASE'),
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      extra: { ssl: true },
+      synchronize: true,
+    };
+  },
+};
